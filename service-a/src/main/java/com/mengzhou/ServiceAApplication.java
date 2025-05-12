0a68fcc321651a4aa279d280b74b60da6519ff13
meng <mengzhou44@gmail.com>
2025-05-11 20:58:43 -0600

    commit

--- service-a/src/main/java/com/mengzhou/ServiceAApplication.java
+++ service-a/src/main/java/com/mengzhou/ServiceAApplication.java
@@ -2,12 +2,20 @@ package com.mengzhou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServiceAApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceAApplication.class, args);
+        var context = SpringApplication.run(ServiceAApplication.class, args);
+
+        Environment env = context.getEnvironment();
+        String[] activeProfiles = env.getActiveProfiles();
+
+        System.out.println("=== Active Spring Profiles: " + Arrays.toString(activeProfiles));
     }
 }
 
